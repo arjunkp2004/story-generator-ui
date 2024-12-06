ae7e50cb6c35a732f5953ebc2aac746d41cbe335
Abhinav <abhinav.kmattul@gmail.com>
2024-12-06 18:40:06 +0530

    Login page small modification

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -26,7 +26,7 @@ export default function Login() {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <div className="bg-white p-8 rounded-lg shadow-lg w-full sm:w-96">
-        <h1 className="text-2xl font-semibold text-center text-gray-700 mb-6">Login</h1>
+        <h1 className="text-2xl font-semibold text-center text-gray-700 mb-6">User Login</h1>
         
         {error && <p className="text-red-500 text-center mb-4">{error}</p>}
         
